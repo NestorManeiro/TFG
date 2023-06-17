1d10ea063f2097c7d5af730328c2583b7a7f6e37
NestorManeiro <91387558+NestorManeiro@users.noreply.github.com>
2023-06-17 16:52:44 +0100

    Resize with shift + wheel when ur moving a circle

--- Utils/tools.js
+++ Utils/tools.js
@@ -17,6 +17,7 @@ function drawFigure() {
         .classed("draggable-circle", true);
 
     draggableCircles.on("mousedown", dragStarted);
+    draggableCircles.on("wheel", changeRadius);
 
     let activeCircle = null;
     let initialX, initialY, deltaX, deltaY;
@@ -45,6 +46,48 @@ function drawFigure() {
 
         activeCircle.setAttribute("cx", newX);
         activeCircle.setAttribute("cy", newY);
+        updateCircleData(activeCircle, newX, newY);
+    }
+
+    function changeRadius(event) {
+        if (!activeCircle) return;
+
+        // Verificar si se mantiene presionada la tecla Shift
+        const isShiftPressed = event.shiftKey;
+
+        if (isShiftPressed) {
+            event.preventDefault(); // Evitar el comportamiento predeterminado del desplazamiento de la página
+
+            const currentRadius = parseFloat(activeCircle.getAttribute("r"));
+            const wheelDelta = event.deltaY;
+
+            const scaleFactor = 1; // Factor de escala para el cambio de radio
+            const deltaRadius = wheelDelta < 0 ? scaleFactor : -scaleFactor; // Cambio positivo o negativo del radio
+
+            const newRadius = Math.max(currentRadius + deltaRadius, 1); // Asegurarse de que el radio no sea menor que 1
+
+            activeCircle.setAttribute("r", newRadius);
+            updateCircleData(activeCircle, null, null, newRadius);
+        }
+    }
+
+    function updateCircleData(circle, newX, newY, newRadius) {
+        const shapeInput = document.getElementById("shapeInput");
+        const circleIndex = Array.from(draggableCircles.nodes()).indexOf(circle);
+        const lines = shapeInput.value.split("\n");
+        const firstLine = lines[0].trim(); // Obtener el valor de la primera línea y eliminar espacios en blanco
+
+        if (newX !== null && newY !== null) {
+            // Actualizar las coordenadas del círculo en el textarea
+            lines[circleIndex + 1] = `${newX} ${newY}`;
+        }
+
+        if (newRadius !== undefined) {
+            // Actualizar el radio del círculo en el textarea
+            lines[circleIndex + parseInt(firstLine)+1] = `${newRadius}`;
+        }
+
+        shapeInput.value = lines.join("\n");
     }
 
     function dragEnded() {
